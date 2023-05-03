dbaaec320480245c36f679c3b151f9d2bff9b72b
Anthony Tleiji <anthonytleiji@gmail.com>
2023-05-03 10:38:48 +0300

    Removed hardcoded user_id from getUserTasks.

--- src/features/task/services/task.service.ts
+++ src/features/task/services/task.service.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
+import jwt_decode from "jwt-decode";
 import { Task } from "../models/task";
 import { NewTask } from "../models/newTask";
+import { DecodedJwt } from "../../auth/models/Jwt";
 
 const getUserTasks = async (token: string | undefined): Promise<Task[]> => {
+  let user_id = 0;
+  if (token) {
+    const decodedJwt: DecodedJwt = jwt_decode(token);
+    user_id = decodedJwt.user.id;
+  }
   const response = await axios.get(
-    // TODO : Replace hardcoded id
-    `http://localhost:3000/users/1/tasks`,
+    `http://localhost:3000/users/${user_id}/tasks`,
     { headers: { Authorization: `Bearer ${token}` } }
   );
   return response.data;
