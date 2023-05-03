e06f322f6767c511a532c3e5f988455f70256840
Anthony Tleiji <anthonytleiji@gmail.com>
2023-05-03 13:59:25 +0300

    Yes

--- src/features/task/models/newTask.ts
+++ src/features/task/models/newTask.ts
@@ -2,7 +2,6 @@ import { TaskStatus } from "./task";
 
 export interface NewTask {
   full_id: string;
-  // TODO: is this needed?
   user_id: number;
   title: string;
   status: TaskStatus;
--- src/features/task/models/task.ts
+++ src/features/task/models/task.ts
@@ -10,7 +10,6 @@ export enum TaskStatus {
 export interface Task {
   id: number;
   full_id: string;
-  // TODO: is this needed?
   user_id: number;
   title: string;
   status: TaskStatus;
