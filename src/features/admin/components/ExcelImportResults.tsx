a6c2a130d5294343164ec29ccc1fb27d399e8f14
Anthony Tleiji <anthonytleiji@gmail.com>
2023-05-08 10:45:08 +0300

    Slight UI changes to import results.

--- src/features/admin/components/ExcelImportResults.tsx
+++ src/features/admin/components/ExcelImportResults.tsx
@@ -1,21 +1,51 @@
-import { Box, Typography } from "@mui/material";
-import { useAppSelector } from "../../../hooks/redux/redux-hooks";
+import React, { useState } from 'react';
+import {
+  Box,
+  Typography,
+  Button,
+  Backdrop,
+  Card,
+  CardContent,
+} from '@mui/material';
+import { useAppSelector } from '../../../hooks/redux/redux-hooks';
 
 export const ExcelImportResults = () => {
   const { importResult } = useAppSelector((state) => state.task);
-  // TODO: display these in a Card (sort of like the score after you win a game)
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Box p={1}>
-      <Typography variant={"h6"}>Import Results</Typography>
-      <Typography variant={"body1"} color={"status.success"}>
-        Success: {importResult.success.length} Tasks
-      </Typography>
-      <Typography variant={"body1"} color={"status.pending"}>
-        Success: {importResult.warnings.length} Tasks
-      </Typography>
-      <Typography variant={"body1"} color={"status.rejected"}>
-        Fails: {importResult.fails.length} Tasks
-      </Typography>
+    <Box>
+      <Button variant="contained" color="secondary" onClick={handleToggle}>
+        View Import Results
+      </Button>
+      <Backdrop
+        open={open}
+        onClick={handleClose}
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Card sx={{ minWidth: 275 }}>
+          <CardContent>
+            <Typography variant="h6">Import Results</Typography>
+            <Typography variant="body1" color="status.success">
+              Success: {importResult.success.length} Tasks
+            </Typography>
+            <Typography variant="body1" color="status.pending">
+              Warnings: {importResult.warnings.length} Tasks
+            </Typography>
+            <Typography variant="body1" color="status.rejected">
+              Fails: {importResult.fails.length} Tasks
+            </Typography>
+          </CardContent>
+        </Card>
+      </Backdrop>
     </Box>
   );
 };
